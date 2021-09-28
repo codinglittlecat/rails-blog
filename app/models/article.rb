b18ddb957b5c3895fe5e4085da8e18e5a82c516e
codinglittlecat <prodanielyang720@gmail.com>
2021-09-27 19:37:54 -0700

    added: state to article&comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true, length: {minimum: 10}
+  include Visible
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
