7701347933f453186ff348d4463aa6af3256f921
codinglittlecat <prodanielyang720@gmail.com>
2021-09-28 06:33:39 -0700

    removed: commenter column

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :status)
+    params.require(:comment).permit(:body, :status)
   end
 end
