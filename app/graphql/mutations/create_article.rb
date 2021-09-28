9d47edcd6434adf75ffc19fbbab2733ea4a39794
codinglittlecat <prodanielyang720@gmail.com>
2021-09-28 02:15:36 -0700

    added: createArticle mutation

---
+++ app/graphql/mutations/create_article.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateArticle < BaseMutation
+    argument :title, String, required: true
+    argument :body, String, required: true
+    argument :status, String, required: false
+
+    type Types::ArticleType
+
+    def resolve(title: nil, body: nil, status: nil)
+      Article.create!(
+        title: title,
+        body: body,
+        status: status
+      )
+    end
+  end
+end
--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -2,5 +2,6 @@ module Types
   class ArticleType < Types::BaseObject
     field :title, String, null: false
     field :body, String, null: false
+    field :status, String, null: true
   end
 end
